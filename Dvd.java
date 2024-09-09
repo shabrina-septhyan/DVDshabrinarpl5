45aaac77e6aafa47d40ae803c57940bbee2831b7
shabrina-septhyan <shabrina_nuraini_33rpl@student.smktelkom-mlg.sch.id>
2024-09-09 14:40:56 +0700

    Add files via upload

---
+++ Dvd.java
@@ -0,0 +1,40 @@
+import modul.Product;
+
+public class Dvd extends Product{
+    
+    private int length;
+    private String rating;
+    private String studio;
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getRating() {
+        return this.rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getStudio() {
+        return this.studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+    
+    // Override dari superclass Product
+    public void print (){
+        super.print();
+        System.out.println("lenght:\t" + length);
+        System.out.println("rating:\t" + rating);
+        System.out.println("studio\t:" + studio);
+}
+}
